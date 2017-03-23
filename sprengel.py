9c332dac7c7282aed8c30180a8f6f20444390d25
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2017-03-23 17:07:39 +0100

    Cleanup sprengel
    Split in multiple subs and improve case folding.

--- sprengel.py
+++ sprengel.py
@@ -5,7 +5,9 @@ import os
 import os.path
 import subprocess
 
-files = [
+
+# Files that exist in any Mercurial repository
+base_files = [
     '.hg/store/00manifest.i',
     '.hg/store/00manifest.d',
     '.hg/store/00changelog.i',
@@ -13,6 +15,7 @@ files = [
     '.hg/dirstate',
     '.hg/requires',
     '.hg/hgrc',
+    '.hg/last-message.txt',
 ]
 
 
@@ -20,6 +23,10 @@ def urljoin(a, b):
     return a + '/' + b
 
 def save_file(url, path):
+    if os.path.isfile(path):
+        print("SKIP: ", path)
+        return
+
     try:
         os.makedirs(os.path.dirname(path))
     except OSError:
@@ -29,28 +36,58 @@ def save_file(url, path):
     if response.ok:
         with open(path, 'wb') as fp:
             fp.write(response.content)
-        print('OK: ', path)
+        print('OK:   ', path)
     else:
         print('FAIL: ', path)
 
 
+def encode_char(c):
+    if c == '_':
+        return '__'
+    if c.isupper():
+        return '_' + c.lower()
+    if ord(c) >= 126 or c in '\:*?"<>|':
+        return '~%x' % ord(c)
+    return c
+
+
 def encode_path(filename):
-    # TODO do case folding
-    return filename.replace('_', '__')
+    return ''.join([encode_char(c) for c in filename])
 
-if __name__ == "__main__":
-    host = sys.argv[1]
 
-    for f in files:
+def download_base_files(host):
+    for f in base_files:
         url = urljoin(host, f)
         save_file(url, f)
 
+
+def get_manifest_file_list():
     manifest = subprocess.check_output(['hg', '--debug', 'manifest'])
+    files = []
     for line in manifest.split('\n'):
-        filename = line[47:]
+        if line:
+            files.append(line[47:])
+    return files
+
+
+def download_manifest_files(files):
+    for filename in files:
         encoded = encode_path(filename)
+        print(encoded)
         for f in ['.hg/store/data/%s.i', '.hg/store/data/%s.d']:
             url = urljoin(host, f % encoded)
             save_file(url, f % filename)
 
 
+def download_hg_directory(host):
+    download_base_files(host)
+    files = get_manifest_file_list();
+    download_manifest_files(files)
+
+
+if __name__ == "__main__":
+    try:
+        host = sys.argv[1]
+        download_hg_directory(host)
+    except IndexError:
+        print("Usage: %s http://example.com/", sys.argv[0])
